f0c7b17ce2b1e480f44169055d5707a0b56eff43
Thrasymachus01 <thrasymachus01@gmail.com>
2020-11-05 15:25:46 +0000

    Add en lang attribute

--- pages/_app.js
+++ pages/_app.js
@@ -17,12 +17,12 @@ export default ({ Component, pageProps }) => {
                 <EthersProvider>
                     <TokenPairProvider>
                         <AccountProvider>
-                            <Head>
+                            {/* <Head>
                                 <title>Interfinex</title>
                                 <link rel="icon" href="/favicon.ico" />
                                 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
                                 <meta name="description" content="Access financial products and DeFi for any ethereum token - The international financial exchange."/>
-                            </Head>
+                            </Head> */}
 
                             <div style={{ display: "grid", gridTemplateRows: "auto 1fr", width: "100%", minHeight: "100%", marginBottom: PIXEL_SIZING.large }} id={"root"}>
                                 <NavBar/>
